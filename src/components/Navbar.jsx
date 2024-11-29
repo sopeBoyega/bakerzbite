896afb18c85919be86956c10c56b6f23b973a0d9
sopeBoyega <sopeadegboyega@gmail.com>
2024-11-29 18:29:37 +0100

    Sope Added text on the Gallery Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,43 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import { list } from "postcss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const listItems = ["Home", "Gallery", "About Us", "Contact Us"];
+  const listItems = ["Home", "Gallery", "About", "Contact"];
 
   const [activeMenu, setActiveMenu] = useState("Home");
 
   return (
-    <div className="border h-[75px] border-black w-[100vw]  z-10 flex justify-between around m-2 items-center bg-transparent fixed ">
+    <div className="border h-[65px] border-black w-[100vw]  z-10 flex justify-between around items-center bg-transparent fixed ">
       <div className="flex flex-row items-center">
-        <p className="text-bakerzBiteDarkBrown  text-[30px] font-medium">
+        <p className="text-bakerzBiteDarkBrown  text-[20px] font-medium">
           BakerzBite
         </p>
         <img
           src={assets.doubleChocolate}
           alt="Logo"
-          className="w-[45px] h-[45px]"
+          className="w-[35px] h-[35px]"
         />
       </div>
      
       <div>
         <ul className="flex  flex-row gap-x-[20px] list-none  ">
           {listItems.map((item, key) => (
-            <li
+            <Link
               key={key}
-              className={` ${activeMenu === item ? "text-[#4A1D1F] font-medium": " text-[#272727]"}  cursor-pointer text-[16px] `}
+              className={` ${activeMenu === item ? "text-[#4A1D1F] font-medium": " text-[#272727]"}  cursor-pointer text-[13px] `}
               onClick={() => setActiveMenu(item)}
+              to={`/${item}`}
             >
               {item}
-            </li>
+            </Link>
           ))}
         </ul>
       </div>
 
       <div className="flex flex-row items-center justify-center mr-10 mb-3">
-        <p className="font-light text-center text-[20px]">Visitor Count:{activeMenu}</p>
+        <p className="font-light text-center text-[10px]">Current Page:{activeMenu}</p>
       </div>
     </div>
   );
---
+++ src/pages/gallery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div>hi I am Miracle the owner of the Gallery Branch!</div>
+  )
+}
+
+export default Gallery
